9c4cb9a5039e829e0cfab66940bc7bed447ca862
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2022-07-13 10:23:17 -0600

    Added routing on button press/moved nimassets to public folder

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const routes = require('./routes/routes')
 const express = require('express');
 
 const app = express();
+const router = express.Router();
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', routes.index);
+app.get('/game', routes.game_screen);
+
+// router.get('/game', (req, res) => {
+//     app.get('/game', routes.game_screen);//adding stuff not so successfully
+// });
+
+router.post('/game', (req, res) => {
+    app.get('/game', routes.game_screen);//adding stuff not so successfully
+});
 
 //DO NOT DO ALL PAGES INTO A SINGLE SCRIPT
 //Using modules to clean it up
--- routes/routes.js
+++ routes/routes.js
@@ -6,3 +6,8 @@ exports.index = (req, res) => {
     //When someone makes a request on the root level of a web server, respond with this function
     res.sendFile(path.join(__dirname, views + '/index.html'));
 };
+
+exports.game_screen = (req, res) => {
+    //When someone makes a request on the root level of a web server, respond with this function
+    res.sendFile(path.join(__dirname, views + '/board.html'));
+};

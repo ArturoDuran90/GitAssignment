f13147a376bb9c6bfa32fb382ad78a4d4124ca56
LethiciaDuncan <lduncan@student.neumont.edu>
2022-07-14 10:21:23 -0600

    Update Code.java

--- public/Code.java
+++ public/Code.java
@@ -1,5 +1,51 @@
-public class Code{
-    public static void main(String[]  args){
+public static void main(String [] args){
+    
+}
+public class Player{
+    String name;
+
+
+    public  void String setName(String newName){
+        this.name = newName;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
+
+public class Game{
+    Player player = new Player();
+    //Going to need to get input
+    player.setName(playerOne)
+    player.setName(playerTwo)
+
+    Arraylist <String> board = new Arraylist <String>;
+
+    public static void declareWinner()
+    {
 
     }
+
+    public static void setTurn()
+{
+
+}
+public static void endTurn()
+{
+
+}
+public static void setGame()
+{
+
+}
+public static void selectPiece()
+{
+
+}
+//do we really need a setter and getter for the players if we can just use player.playerOne()
+public static void resetBoard()
+{
+
+}
 }
